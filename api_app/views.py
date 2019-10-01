bf87843bef5fd7fd7b4d47698bf954c9dc2c5f65
Muazzam Mullah <zmz.muazzammullah@gmail.com>
2019-10-01 19:22:45 +0200

    Update views.py

--- api_app/views.py
+++ api_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+import json
 #########################
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,6 +10,12 @@ from rest_framework import status
 from rest_framework import serializers
 ########################
 from .models import Employee
+
+def index_api_response(request):
+	responseData = {
+    	'Error':'Endpoint not found:Append Endpoint' ,
+	}
+	return HttpResponse(json.dumps(responseData), content_type="application/json")
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
